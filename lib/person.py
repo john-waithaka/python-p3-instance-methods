0eeba9a847d4161d192110af5799c10f44466aad
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-07-10 23:03:16 +0300

    class instance

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
+# class Person:
+#     # Class body goes here
 
-    #Instance method definition
-    pass
+#     #Instance method definition
+#     pass
+
+
+
+class Person:
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
