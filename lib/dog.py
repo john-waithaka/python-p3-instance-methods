2c2cd37eeea9f12515accfd3b9a0d9ba8f7d6d9b
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-07-14 23:54:48 +0300

    instance working correct

--- lib/dog.py
+++ lib/dog.py
@@ -6,6 +6,10 @@
 #     #Instance method definition
 #     pass
 
+# Complete the following tasks to get the rest of the tests passing:
+
+# Add an instance method sit() to your Dog class that will print "The dog is sitting."
+# Define a Person class in lib/person.py
 
 
 class Dog:
@@ -14,3 +18,10 @@ class Dog:
 
     def sit(self):
         print("The dog is sitting.")
+
+
+dog1 = Dog()
+
+dog1.bark()
+
+dog1.sit()
--- lib/person.py
+++ lib/person.py
@@ -6,6 +6,10 @@
 #     #Instance method definition
 #     pass
 
+# Complete the following tasks to get the rest of the tests passing:
+
+# Add an instance method talk() to your Person class that will print "Hello World!"
+# Add an instance method walk() to your Person class that will print "The person is walking."
 
 
 class Person:
@@ -14,3 +18,10 @@ class Person:
     
     def walk(self):
         print("The person is walking.")
+
+person1 = Person()
+
+person1.talk()
+person1.walk()
+
+
